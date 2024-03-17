ed58b8ea337e99e43838fca8f215c749bebea4db
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-18 01:32:38 +0600

    footer added

--- AngularComponent/eShopping/src/app/notification/notification.component.ts
+++ AngularComponent/eShopping/src/app/notification/notification.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div>
-                 <p> This website uses cookies to provide better use experience.</p>
-               </div>`,
-  styleUrl: './notification.component.css'
+  template: `<div style="background-color: #07878b; color: #f8f1f1e7; font-family: Arial, sans-serif;
+                         display: flex; justify-content: center; align-items: center; height: 30px;margin-top:-10px">
+                 <p> This website uses cookies to provide a better user experience.</p>
+               </div>`
 })
 export class NotificationComponent {
 
