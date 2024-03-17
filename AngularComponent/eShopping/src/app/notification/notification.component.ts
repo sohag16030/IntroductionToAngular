568b64af0fd30f4a7cef9090d34e6a04a86e76d1
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-18 01:10:04 +0600

    header image and text alignement is fix now

---
+++ AngularComponent/eShopping/src/app/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<div>
+                 <p> This website uses cookies to provide better use experience.</p>
+               </div>`,
+  styleUrl: './notification.component.css'
+})
+export class NotificationComponent {
+
+}
