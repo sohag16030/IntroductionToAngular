0627dc36738009f951220e27ee41df873bde097c
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-18 14:52:49 +0600

    search component added wtih eshopping

---
+++ AngularComponent/eShopping/src/app/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+    searchValue:string = '';
+    onInputChange(event: any): void {
+      this.searchValue = event.target.value;
+      console.log(this.searchValue);
+    }
+    search(): void {
+      console.log(this.searchValue);
+      // You can perform additional logic here, such as making an API request with the searchValue
+    }
+}
