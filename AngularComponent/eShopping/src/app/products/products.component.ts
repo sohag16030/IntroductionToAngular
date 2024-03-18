858d72cfbe315e924e5e20866856d549649d7112
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-18 23:07:47 +0600

    ngFor used for itaration and ngStyle is used for change the css

--- AngularComponent/eShopping/src/app/products/products.component.ts
+++ AngularComponent/eShopping/src/app/products/products.component.ts
@@ -9,24 +9,28 @@ export class ProductsComponent {
   products: any[] = [
     {
       name: 'T-Shirt',
+      stock: 'Available',
       description: 'Comfortable cotton T-shirt with brand logo',
       price: 20.99,
       image: 'assets/img/product/thsirt.jpg',
     },
     {
       name: 'Hoodie',
+      stock: 'Available',
       description: 'Warm hoodie with front pocket and drawstrings',
       price: 35.99,
       image: 'assets/img/product/hoodie.jpg',
     },
     {
       name: 'Jeans',
+      stock: 'Stock Out',
       description: 'Classic denim jeans with a modern fit',
       price: 45.99,
       image: 'assets/img/product/jeans.jpg',
     },
     {
       name: 'Jeans',
+      stock: 'Available',
       description: 'Classic denim jeans with a modern fit',
       price: 40.99,
       image: 'assets/img/product/jeans2.jpg',
