da390c84e68861d0a015ac8a8dde30a6a22f3657
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-18 15:38:35 +0600

    event binding is not working for me ... view to component data gettting

--- AngularComponent/eShopping/src/app/notification/notification.component.ts
+++ AngularComponent/eShopping/src/app/notification/notification.component.ts
@@ -1,27 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { flatMap } from 'rxjs';
 
 @Component({
   selector: 'app-notification',
-  template: `<div [hidden]="displayNotification" class="notification">
-                 <p> This website uses cookies to provide a better user experience.</p>
-               </div>`,
-  styles: [`
-    .notification {
-      background-color: #07878b;
-      color: #f8f1f1e7;
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 30px;
-    }
-  `]
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css'] 
 })
-export class NotificationComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class NotificationComponent  {
+  isHidden: boolean = false;
   
-  displayNotification:boolean = true;
+  hideNotificationBar(){
+    console.log("button clicked");
+    this.isHidden = true;
+    console.log(this.isHidden);
+  }
+  
 }
