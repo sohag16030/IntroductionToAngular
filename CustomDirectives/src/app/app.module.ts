10bf005b870547d5e2742783cff4416900613d5d
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-24 12:43:57 +0600

    @HostBinding is used for customize the html element like style and @HostListner is used for listen the event that will perform by user like mouse hover or others

--- CustomDirectives/src/app/app.module.ts
+++ CustomDirectives/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HoverDirective } from './hostListner/hover.directive';
 import { BackgroundStyleDirective } from './hostListner/background-style.directive';
+import { BetterhighlightDirective } from './hostBinding/betterhighlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HoverDirective,
-    BackgroundStyleDirective
+    BackgroundStyleDirective,
+    BetterhighlightDirective
   ],
   imports: [
     BrowserModule,
