0eb7b450df4913e17db90691588e9c659a425044
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-26 22:27:41 +0600

    navigate programatically  by Url

--- Angular_Routes/src/app/about/about.component.ts
+++ Angular_Routes/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  constructor(private router: Router) { }
+
+  navigateToHomePage() {
+    this.router.navigateByUrl('Home');
+  }
 }
