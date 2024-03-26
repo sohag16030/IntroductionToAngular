2c33418d86239f8bab1bd9b4fda14876d772340a
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-26 10:43:24 +0600

    Routes configuration is working fine.

--- Angular_Routes/src/app/app.module.ts
+++ Angular_Routes/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+const appRoute:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Contact',component:ContactComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,6 +23,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
