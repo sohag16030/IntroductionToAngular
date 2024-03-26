b474dac1c33090db71752d0ada0821a55db3ce60
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-27 00:05:01 +0600

    everyting is working properly now

--- Angular_Routes/src/app/services/courses.service.ts
+++ Angular_Routes/src/app/services/courses.service.ts
@@ -2,57 +2,39 @@ export class CoursesService {
     courses = [
         {
             id: 1,
-            name: 'Web Development Fundamentals',
-            image: 'web_dev.png',
+            name: 'Java Script',
+            image: 'assets/javascript.png',
             description: 'Learn the basics of web development, including HTML, CSS, and JavaScript.'
         },
         {
             id: 2,
-            name: 'Introduction to Python Programming',
-            image: 'python_intro.png',
+            name: 'Angular',
+            image: 'assets/angular.png',
             description: 'Get started with Python programming language and learn its fundamentals.'
         },
         {
             id: 3,
-            name: 'JavaScript Advanced Techniques',
-            image: 'javascript_advanced.png',
+            name: 'React',
+            image: 'assets/react.png',
             description: 'Dive deeper into JavaScript programming with advanced techniques and concepts.'
         },
         {
             id: 4,
-            name: 'Angular Framework Essentials',
-            image: 'angular_essentials.png',
-            description: 'Master the essentials of Angular framework for building modern web applications.'
+            name: 'Java Script',
+            image: 'assets/javascript.png',
+            description: 'Learn the basics of web development, including HTML, CSS, and JavaScript.'
         },
         {
             id: 5,
-            name: 'ReactJS Fundamentals',
-            image: 'reactjs_fundamentals.png',
-            description: 'Learn the fundamentals of ReactJS library for building user interfaces.'
+            name: 'Angular',
+            image: 'assets/angular.png',
+            description: 'Get started with Python programming language and learn its fundamentals.'
         },
         {
             id: 6,
-            name: 'Node.js Backend Development',
-            image: 'nodejs_backend.png',
-            description: 'Build scalable and efficient backend applications with Node.js runtime environment.'
-        },
-        {
-            id: 7,
-            name: 'Data Science with Python',
-            image: 'data_science_python.png',
-            description: 'Explore data science concepts and techniques using Python programming language.'
-        },
-        {
-            id: 8,
-            name: 'Machine Learning Foundations',
-            image: 'machine_learning_foundations.png',
-            description: 'Get started with machine learning and understand its foundational concepts.'
-        },
-        {
-            id: 9,
-            name: 'Cloud Computing Essentials',
-            image: 'cloud_computing.png',
-            description: 'Learn about cloud computing fundamentals and popular cloud platforms.'
+            name: 'React',
+            image: 'assets/react.png',
+            description: 'Dive deeper into JavaScript programming with advanced techniques and concepts.'
         }
     ];
 
