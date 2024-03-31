6cb2f48c92ab890bd597242cf3ba83e29f802bfd
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-04-01 00:36:14 +0600

    post /get working

--- EMS_FRONTEND/src/app/app-routing.module.ts
+++ EMS_FRONTEND/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
-const routes: Routes = [];
+const appRoute: Routes = [
+  { path: '', component: EmployeeListComponent},
+  { path: 'AddEmployee', component: AddEmployeeComponent},
+  { path: '**', component: ErrorComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoute)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- EMS_FRONTEND/src/app/app.module.ts
+++ EMS_FRONTEND/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
