b55da840a91e690ba91d1579c75f69aedbcdff09
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2024-03-31 15:11:37 +0600

    Spring boot demo project added

---
+++ EMS/src/main/java/com/example/EMS/Employee.java
@@ -0,0 +1,51 @@
+package com.example.EMS;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "EMS_USER")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Column(name = "MOBILE_NUMBER", unique = true, nullable = false)
+    private String mobileNumber;
+
+    @Column(name = "EMAIL", unique = true)
+    private String email;
+
+    @Column(name = "IS_ACTIVE", nullable = false)
+    private Boolean isActive;
+
+    @Column(name = "CREATED_AT", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "UPDATED_AT", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ EMS/src/main/java/com/example/EMS/service.java
@@ -0,0 +1,4 @@
+package com.example.EMS;
+
+public class service {
+}
